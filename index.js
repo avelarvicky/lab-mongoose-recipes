17a498c4523a9684d4af39285ea2340d0f6fcdcc
avelarvicky <victoriahelenaavelar@gmail.com>
2023-05-04 17:44:17 +0100

    Lab done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 //Method 1 : Using Async Await
 
@@ -19,6 +19,35 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
+    let createdRecipe = {
+      title: "Chocoghetti",
+      level: "Amateur Chef",
+      ingredients: ["Spaghetti", "Dark Chocolate"],
+      cuisine: "Fast food",
+      dishType: "soup",
+      duration: 0.4,
+      creator: "Victoria Avelar",
+    };
+    await Recipe.create(createdRecipe);
+
+    let recipe = await Recipe.find({ title: "Chocoghetti" });
+    console.log(recipe[0].title);
+
+    await Recipe.insertMany(data)
+    let allRecipes = await Recipe.find()
+
+    let titles = [];
+    for(let i=0; i<allRecipes.length; i++){
+      titles.push(allRecipes[i].title)
+    }
+    console.log(titles);
+
+    await Recipe.findOneAndUpdate({name: 'Rigatoni alla Genovese'}, {duration: 100});
+    console.log('Success');
+
+    await Recipe.deleteOne({title: 'Carrot Cake'});
+    console.log('Success');
+
   } catch (error) {
     console.log(error);
   }
